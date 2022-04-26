423b60d80bd9bba4ccfd29e152091e41bf9dfe65
Thuan Dev <ngovanthuan07@gmail.com>
2022-04-26 07:56:39 +0700

    BT DIEM DANH 26-4

---
+++ Bai2TruuTuong/Bai2TruuTuong/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Bai2TruuTuong
+{
+    class Animal
+    {
+        public string eat()
+        {
+            return "";
+        }
+
+        public string makeSound()
+        {
+            return "";
+        }
+    }
+
+    class Cat:Animal
+    {
+        public string eat()
+        {
+            return "Meo an ca";
+        }
+
+        public string makeSound()
+        {
+            return "Meo meo";
+        }
+
+        public void run()
+        {
+            Console.WriteLine("Cat: " + eat() + makeSound());
+        }
+    }
+
+    class Bird : Animal
+    {
+        public string eat()
+        {
+            return "Chim an sau";
+        }
+
+        public string makeSound()
+        {
+            return "Chip chip";
+        }
+
+        public void fly()
+        {
+            Console.WriteLine("Bird: " + eat() + makeSound());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.run();
+            Bird bird = new Bird();
+            bird.fly();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ PhuongTrinhBac2/PhuongTrinhBac2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace PhuongTrinhBac2
+{
+    class PhuongTrinhBac2
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public double A { get => a; set => a = value; }
+        public double B { get => b; set => b = value; }
+        public double C { get => c; set => c = value; }
+
+        public PhuongTrinhBac2(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public PhuongTrinhBac2()
+        {
+        }
+
+        public double delta()
+        {
+            return b * b - 4 * a * c; 
+        }
+        
+        public void nghiemPhuongTrinh()
+        {
+            if (delta() > 0)
+            {
+                Console.WriteLine("Phuong trinh co hai nghiem : ");
+                Console.WriteLine("X1 = {0}", ((-b - Math.Sqrt(delta())) / 2 * a));
+                Console.WriteLine("X2 = {0}", ((-b + Math.Sqrt(delta())) / 2 * a));
+
+            }
+            else if (delta() == 0)
+            {
+                Console.WriteLine("Phuong trinh co hai nghiem kep nghiem");
+                Console.WriteLine("X1 = X2 {0}", -b / 2 * a);
+            }
+            else if (delta() < 0)
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap phuong trinh bac 2: ");
+            PhuongTrinhBac2 phuongTrinhBac2 = new PhuongTrinhBac2();
+            Console.WriteLine("Nhap a: ");
+            phuongTrinhBac2.A = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap b: ");
+            phuongTrinhBac2.B = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap c: ");
+            phuongTrinhBac2.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ket qua: ");
+
+            phuongTrinhBac2.nghiemPhuongTrinh();
+
+            Console.ReadKey();
+        }
+    }
+}
